d3fe7fd0f084abb2289e04ad204b058941b8e505
Alicia VB <152905690+aliciavb@users.noreply.github.com>
2025-01-15 13:44:23 +0100

    Add files via upload

---
+++ Libft/libft.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avinals- <avinals-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 12:13:24 by avinals-          #+#    #+#             */
+/*   Updated: 2025/01/15 13:31:57 by avinals-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int ft_isprint(int c);
+
+/* faltan;
+• strlen
+• memset
+• bzero
+• memcpy
+• memmove
+• strlcpy
+• strlcat
+• toupper
+• tolower
+• strchr
+• strrchr
+• strncmp
+• memchr
+• memcmp
+• strnstr
+• atoi
+• calloc 
+• strdup
+*/
+
+#endif

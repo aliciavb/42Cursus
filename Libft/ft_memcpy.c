b02a37821e91a0e2cda779bb03dcd4ca2c96e917
Alicia VB <152905690+aliciavb@users.noreply.github.com>
2025-01-16 19:29:55 +0100

    Add files via upload

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avinals- <avinals-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 19:18:25 by avinals-          #+#    #+#             */
+/*   Updated: 2025/01/16 19:20:38 by avinals-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char *d;
+	unsigned char *s;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}

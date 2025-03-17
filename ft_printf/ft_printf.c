1979ccb699b19222ddcd0f1face7a0fb17e38616
Alicia VB <152905690+aliciavb@users.noreply.github.com>
2025-03-17 18:30:50 +0100

    correctly print no % and string wip

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -3,106 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avinals <avinals-@student.42madrid.com>    +#+  +:+       +#+        */
+/*   By: avinals- <avinals-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:48:41 by avinals           #+#    #+#             */
-/*   Updated: 2025/03/13 11:48:41 by avinals          ###   ########.fr       */
+/*   Updated: 2025/03/17 18:28:39 by avinals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-char *ft_parse_format(const char **format, va_list args)
+int	putstr(const char *str)
 {
-	char *str;
-	int count;
-
-	count = 0;
-	if (**format == 's')
-		str = ft_strdup(va_arg(args, char *));
-	else if (**format == 'd')
-		str = ft_itoa(va_arg(args, int));
-	else if (**format == 'x')
-		str = ft_itoa_base(va_arg(args, unsigned int), "0123456789abcdef");
-	else if (**format == 'X')
-		str = ft_itoa_base(va_arg(args, unsigned int), "0123456789ABCDEF");
-	else
+	int i;
+	
+	i = 0;
+	while (str[i] != '\0')
 	{
-		str = (char *)malloc(2);
-		str[0] = **format;
-		str[1] = '\0';
+		write(1, &str[i], 1);
+		i++;
 	}
-	(*format)++;
-	return (str);
+	return(i);
 }
 
-/* ft_vprintf handles formatted output with a variable number
- of arguments, leveraging the va_list type to manage arguments dynamically.
- This function is used to create custom formatted output functions
- that can handle a flexible number of input parameters. */
-int ft_vprintf(const char *format, va_list args)
+int	check_format(char const *str, va_list list)
 {
 	int count;
-	char *str;
+	
+	count = 0;
+	while (*str)
+	{
+		/* if (str[i] == 'c')
+			//putchar; */
+		if (*str == 's')
+			count = putstr(va_arg(list, char *));
+		/* if (str[i] == 'd')
+			//putnbr;
+		if (str[i] == 'u')
+			//put unsigned nbr;
+		if (str[i] == 'p')
+			//putnbr with pointer base; */
+		str++;
+	}
+	return(count);
+}
+
+int	ft_printf(char const *str, ...)
+{
+	va_list	list;
+	int		count;
 
 	count = 0;
-	while (*format)
+	va_start(list, str);
+	while (*str)
 	{
-		if (*format == '%')
+		if (*str == '%')
 		{
-			format++;
-			str = ft_parse_format(&format, args);
-			count += ft_putstr(str);
-			free(str);
+			str++;
+			if (*str)
+				count = count + (check_format(str, list));
 		}
 		else
 		{
-			count += ft_putchar(*format);
-			format++;
+			write(1, str++, 1);
+			count++;
 		}
 	}
-	return (count);
-}
-
-int ft_printf(const char *format, ...)
-{
-	va_list args;
-	int count;
-
-	va_start(args, format);
-	count = ft_vprintf(format, args);
-	va_end(args);
+	va_end(list);
 	return (count);
 }
 
 int main(void)
 {
-	ft_printf("Hello, %s! %d %x %X\n", "world", 42, 42, 42);
-	printf("Hello, %s! %d %x %X\n", "world", 42, 42, 42);
-
-	ft_printf("Character: %c\n", 'A');
-	printf("Character: %c\n", 'A');
-
-	ft_printf("String: %s\n", "Test string");
-	printf("String: %s\n", "Test string");
-
-	ft_printf("Pointer: %p\n", (void *)0x12345678);
-	printf("Pointer: %p\n", (void *)0x12345678);
-
-	ft_printf("Decimal: %d\n", 12345);
-	printf("Decimal: %d\n", 12345);
-
-	ft_printf("Integer: %i\n", -12345);
-	printf("Integer: %i\n", -12345);
-
-	ft_printf("Unsigned: %u\n", 12345);
-	printf("Unsigned: %u\n", 12345);
-
-	ft_printf("Hexadecimal (lowercase): %x\n", 0xabcdef);
-	printf("Hexadecimal (lowercase): %x\n", 0xabcdef);
-
-	ft_printf("Hexadecimal (uppercase): %X\n", 0xABCDEF);
-	printf("Hexadecimal (uppercase): %X\n", 0xABCDEF);
-
-	return (0);
+	//check no %
+	printf("-%d\n", ft_printf("str\n"));
+	printf("-%d\n", printf("str\n"));
+	
+	//check %s
+	printf("-%d\n", ft_printf("%s", "hello\n"));
+	printf("-%d\n", printf("%s", "hello\n"));
 }
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: avinals <avinals-@student.42madrid.com>    +#+  +:+       +#+        */
+/*   By: avinals- <avinals-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:50:04 by avinals           #+#    #+#             */
-/*   Updated: 2025/03/13 11:50:04 by avinals          ###   ########.fr       */
+/*   Updated: 2025/03/17 14:17:14 by avinals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include <stdarg.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <unistd.h>
 
-int		ft_printf(const char *format, ...);
-int		ft_vprintf(const char *format, va_list args);
+int	ft_printf(char const *format, ...);
 
 #endif

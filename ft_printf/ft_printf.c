dee80a5bac3951f93473f938099ad7649ce59062
Alicia Vinals Ballester <avinals-@c2r15s1.42madrid.com>
2025-03-24 16:57:46 +0100

    works with cspdiuxX%

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,62 +6,11 @@
 /*   By: avinals- <avinals-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:48:41 by avinals           #+#    #+#             */
-/*   Updated: 2025/03/24 14:18:16 by avinals-         ###   ########.fr       */
+/*   Updated: 2025/03/24 15:06:32 by avinals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	ft_putchar(char c, int *count)
-{
-	write(1, &c, 1);
-	(*count)++;
-}
-
-void	ft_putstr(const char *str, int *count)
-{
-	while (*str)
-	{
-		ft_putchar(*str, count);
-		str++;
-	}
-}
-
-void	ft_putnbr(int nbr, int *count)
-{
-	if (nbr == INT_MIN)
-	{
-		ft_putstr("-2147483648", count);
-		return ;
-	}
-	if (nbr < 0)
-	{
-		ft_putchar('-', count);
-		nbr = -nbr;
-	}
-	if (nbr > 9)
-	{
-		ft_putnbr(nbr / 10, count);
-	}
-	ft_putchar((nbr % 10) + '0', count);
-	return ;
-}
-
-void	ft_putnbr_base(unsigned long str, char *base, int *count)
-{
-	if (str >= 16)
-		ft_putnbr_base(str / 16, base, count);
-	ft_putchar(base[str % 16], count);
-}
-
-void	ft_putp(void *str, int *count)
-{
-	long int	long_str;
-
-	long_str = (unsigned long)str;
-	ft_putstr("0x", count);
-	ft_putnbr_base(long_str, BASE_LOWER, count);
-}
 
 /* formats to check: c s p d i u x X % */
 int	check_format(va_list list, char str, int *count)
@@ -74,12 +23,14 @@ int	check_format(va_list list, char str, int *count)
 		ft_putp(va_arg(list, void *), count);
 	else if (str == 'd' || str == 'i')
 		ft_putnbr(va_arg(list, int), count);
-	/*else if (str == 'u')
-		//
+	else if (str == 'u')
+		ft_putu(va_arg(list, unsigned int), count);
 	else if (str == 'x')
-		//
+		ft_putlower(va_arg(list, unsigned int), count);
 	else if (str == 'X')
-		//*/
+		ft_putupper(va_arg(list, unsigned int), count);
+	else if (str == '%')
+		ft_putchar('%', count);
 	return (*count);
 }
 
@@ -114,27 +65,45 @@ int	ft_printf(char const *str, ...)
 /* formats to check: c s p d i u x X % */
 /* int main(void)
 {
-	//check no %
+	// check no %
 	printf("-%d\n", ft_printf("str-->"));
 	printf("-%d\n", printf("str-->"));
-	
-	//check %c
+
+	// check %c
 	printf("-%d\n", ft_printf("%c", 'a'));
 	printf("-%d\n", printf("%c", 'a'));
-	
-	//check %s
+
+	// check %s
 	printf("-%d\n", ft_printf("%s", "hello-->"));
 	printf("-%d\n", printf("%s", "hello-->"));
-	
-	//check %p
+
+	// check %p
 	printf("-%d\n", ft_printf("%p", &ft_printf));
 	printf("-%d\n", printf("%p", &ft_printf));
-	
-	//check %d
+
+	// check %d
 	printf("-%d\n", ft_printf("%d", 12));
 	printf("-%d\n", printf("%d", 12));
-	
-	//check %i
+
+	// check %i
 	printf("-%d\n", ft_printf("%i", 012));
 	printf("-%d\n", printf("%i", 012));
+
+	// check %u
+    printf("-%d\n", ft_printf("%u", 1234567890));
+    printf("-%d\n", printf("%u", 1234567890));
+
+    // check %x
+    printf("-%d\n", ft_printf("%x", 255));
+    printf("-%d\n", printf("%x", 255));
+
+    // check %X
+    printf("-%d\n", ft_printf("%X", 255));
+    printf("-%d\n", printf("%X", 255));
+
+    // check %%
+    printf("-%d\n", ft_printf("%%"));
+    printf("-%d\n", printf("%%"));
+
+    return 0;
 } */
--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: avinals- <avinals-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:50:04 by avinals           #+#    #+#             */
-/*   Updated: 2025/03/24 14:06:15 by avinals-         ###   ########.fr       */
+/*   Updated: 2025/03/24 15:05:36 by avinals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,14 @@
 # define BASE_UPPER "0123456789ABCDEF"
 # define BASE_LOWER "0123456789abcdef"
 
-int	ft_printf(char const *format, ...);
+int		ft_printf(char const *format, ...);
+void	ft_putchar(char c, int *count);
+void	ft_putstr(const char *str, int *count);
+void	ft_putnbr(int nbr, int *count);
+void	ft_putu(unsigned int nbr, int *count);
+void	ft_putnbr_base(unsigned long str, char *base, int *count);
+void	ft_putp(void *str, int *count);
+void	ft_putlower(unsigned int nbr, int *count);
+void	ft_putupper(unsigned int nbr, int *count);
 
 #endif

e0ee9c90ae3fb0b999978400bbd78a6d6c69b9cf
Alicia VB <152905690+aliciavb@users.noreply.github.com>
2025-03-25 13:19:35 +0100

    ready for evaluation

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: avinals- <avinals-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:48:41 by avinals           #+#    #+#             */
-/*   Updated: 2025/03/24 15:06:32 by avinals-         ###   ########.fr       */
+/*   Updated: 2025/03/25 13:14:49 by avinals-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,14 +43,11 @@ int	ft_printf(char const *str, ...)
 	va_start(list, str);
 	while (*str)
 	{
-		if (*str == '%')
+		if (*str == '%' && *(str + 1))
 		{
 			str++;
-			if (*str)
-			{
-				check_format(list, *str, &count);
-				str++;
-			}
+			check_format(list, *str, &count);
+			str++;
 		}
 		else
 		{
@@ -61,49 +58,3 @@ int	ft_printf(char const *str, ...)
 	va_end(list);
 	return (count);
 }
-
-/* formats to check: c s p d i u x X % */
-/* int main(void)
-{
-	// check no %
-	printf("-%d\n", ft_printf("str-->"));
-	printf("-%d\n", printf("str-->"));
-
-	// check %c
-	printf("-%d\n", ft_printf("%c", 'a'));
-	printf("-%d\n", printf("%c", 'a'));
-
-	// check %s
-	printf("-%d\n", ft_printf("%s", "hello-->"));
-	printf("-%d\n", printf("%s", "hello-->"));
-
-	// check %p
-	printf("-%d\n", ft_printf("%p", &ft_printf));
-	printf("-%d\n", printf("%p", &ft_printf));
-
-	// check %d
-	printf("-%d\n", ft_printf("%d", 12));
-	printf("-%d\n", printf("%d", 12));
-
-	// check %i
-	printf("-%d\n", ft_printf("%i", 012));
-	printf("-%d\n", printf("%i", 012));
-
-	// check %u
-    printf("-%d\n", ft_printf("%u", 1234567890));
-    printf("-%d\n", printf("%u", 1234567890));
-
-    // check %x
-    printf("-%d\n", ft_printf("%x", 255));
-    printf("-%d\n", printf("%x", 255));
-
-    // check %X
-    printf("-%d\n", ft_printf("%X", 255));
-    printf("-%d\n", printf("%X", 255));
-
-    // check %%
-    printf("-%d\n", ft_printf("%%"));
-    printf("-%d\n", printf("%%"));
-
-    return 0;
-} */

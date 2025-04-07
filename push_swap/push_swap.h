7388cc79905d50ab21b98cc8ab1d74a148229f72
Alicia VB <152905690+aliciavb@users.noreply.github.com>
2025-04-07 14:45:07 +0200

    Add files via upload

---
+++ push_swap/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: avinals <avinals-@student.42madrid.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 13:59:40 by avinals           #+#    #+#             */
+/*   Updated: 2025/04/07 14:00:26 by avinals          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP
+#define PUSH_SWAP
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+#endif

60f1dfb05853cadc17d9085f40466a9e367169e6
Alicia VB <152905690+aliciavb@users.noreply.github.com>
2025-07-10 13:21:15 +0200

    fixed valgrind for ac == 2

--- push_swap/main/push_swap.c
+++ push_swap/main/push_swap.c
@@ -12,18 +12,47 @@
 
 #include "push_swap.h"
 
+void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+static int	handle_input(int ac, char **av, t_stack_node **a,
+	char ***split_args)
+{
+	if (ac == 2)
+	{
+		*split_args = split(av[1], ' ');
+		if (!*split_args)
+			return (1);
+		init_stack_a(a, *split_args);
+	}
+	else
+		init_stack_a(a, av + 1);
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack_node	*a;
 	t_stack_node	*b;
+	char			**split_args;
 
 	a = NULL;
 	b = NULL;
+	split_args = NULL;
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
-	else if (ac == 2)
-		av = split(av[1], ' ');
-	init_stack_a(&a, av + 1);
+	if (handle_input(ac, av, &a, &split_args))
+		return (1);
 	if (!stack_sorted(a))
 	{
 		if (stack_len(a) == 2)
@@ -34,5 +63,7 @@ int	main(int ac, char **av)
 			sort_stacks(&a, &b);
 	}
 	free_stack(&a);
+	if (split_args)
+		free_split(split_args);
 	return (0);
 }

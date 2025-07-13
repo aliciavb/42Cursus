371842ef8dd2b90033b42ec035111997b1cfde48
aliciavb <ali.vinals@gmail.com>
2025-07-13 12:43:07 +0200

    working with all tests

--- minitalk/server.c
+++ minitalk/server.c
@@ -6,113 +6,83 @@
 /*   By: avinals <avinals-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 12:31:43 by avinals           #+#    #+#             */
-/*   Updated: 2025/07/12 18:14:22 by avinals          ###   ########.fr       */
+/*   Updated: 2025/07/13 18:00:00 by avinals          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static char	*g_buffer = NULL;
+static char	g_message[4096];
 
-void	reset_state(int *buffer_len, int *buffer_size, 
-                int *bit_index, unsigned char *current_char, pid_t *client)
+void	print_message(void)
 {
-    if (g_buffer)
-    {
-        free(g_buffer);
-        g_buffer = NULL;
-    }
-    *buffer_len = 0;
-    *buffer_size = 0;
-    *bit_index = 0;
-    *current_char = 0;
-    *client = 0;
+	ft_printf("%s\n", g_message);
 }
 
-void	expand_buffer(int *buffer_len, int *buffer_size, 
-                    int *bit_index, unsigned char *current_char, pid_t *client)
+void	reset_message(void)
 {
-    char	*new_buffer;
+	int	i;
 
-    *buffer_size = *buffer_size == 0 ? 1024 : *buffer_size * 2;
-    new_buffer = malloc(*buffer_size);
-    if (!new_buffer)
-    {
-        reset_state(buffer_len, buffer_size, bit_index, current_char, client);
-        return ;
-    }
-    if (g_buffer)
-    {
-        ft_memcpy(new_buffer, g_buffer, *buffer_len);
-        free(g_buffer);
-    }
-    g_buffer = new_buffer;
+	i = 0;
+	while (i < 4096)
+	{
+		g_message[i] = '\0';
+		i++;
+	}
 }
 
-void	process_char(unsigned char c, int *buffer_len, int *buffer_size, 
-                    int *bit_index, unsigned char *current_char, pid_t *client)
+void	add_char_to_message(unsigned char c)
 {
-    if (c == '\0')
-    {
-        if (g_buffer && *buffer_len > 0)
-        {
-            g_buffer[*buffer_len] = '\0';
-            ft_printf("%s\n", g_buffer);
-        }
-        reset_state(buffer_len, buffer_size, bit_index, current_char, client);
-    }
-    else
-    {
-        if (*buffer_len >= *buffer_size)
-            expand_buffer(buffer_len, buffer_size, bit_index, current_char, client);
-        if (g_buffer)
-            g_buffer[(*buffer_len)++] = c;
-    }
+	static int	len = 0;
+
+	if (c == '\0')
+	{
+		g_message[len] = '\0';
+		print_message();
+		reset_message();
+		len = 0;
+		return ;
+	}
+	if (len < 4095)
+	{
+		g_message[len] = c;
+		len++;
+	}
 }
 
-void	ft_handler(int signal, siginfo_t *info, void *context)
+void	ft_handler(int signal)
 {
-    static unsigned char	current_char = 0;
-    static int				bit_index = 0;
-    static int				buffer_len = 0;
-    static int				buffer_size = 0;
-    static pid_t			active_client = 0;
+	static unsigned char	current_char = 0;
+	static int				bit_index = 0;
 
-    (void)context;
-    if (active_client != 0 && active_client != info->si_pid)
-        return ;
-    if (active_client == 0)
-        active_client = info->si_pid;
-    current_char = (current_char << 1) | (signal == SIGUSR1);
-    bit_index++;
-    if (bit_index == 8)
-    {
-        process_char(current_char, &buffer_len, &buffer_size, 
-            &bit_index, &current_char, &active_client);
-        bit_index = 0;
-        current_char = 0;
-    }
+	if (signal == SIGUSR1)
+		current_char = (current_char << 1) | 1;
+	else
+		current_char = current_char << 1;
+	bit_index++;
+	if (bit_index == 8)
+	{
+		add_char_to_message(current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
 }
 
 int	main(int ac, char **av)
 {
-    struct sigaction	sa;
-
-    (void)av;
-    if (ac != 1)
-    {
-        ft_printf("Error. Try: ./server\n");
-        return (1);
-    }
-    ft_printf("PID: %d\n", getpid());
-    ft_printf("Messages appear below\n");
-    ft_printf("───────────────────────────────────────────────\n");
-    sa.sa_sigaction = ft_handler;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_SIGINFO;
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-    while (1)
-        pause();
-    return (0);
+	(void)av;
+	if (ac != 1)
+	{
+		ft_printf("Error. Try: ./server\n");
+		return (1);
+	}
+	ft_printf("PID: %d\n", getpid());
+	ft_printf("Messages appear below\n");
+	ft_printf("───────────────────────────────────────────────\n");
+	reset_message();
+	signal(SIGUSR1, ft_handler);
+	signal(SIGUSR2, ft_handler);
+	while (1)
+		pause();
+	return (0);
 }
--- minitalk/server_bonus.c
+++ minitalk/server_bonus.c
@@ -6,13 +6,73 @@
 /*   By: avinals <avinals-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 12:32:57 by avinals           #+#    #+#             */
-/*   Updated: 2025/07/12 17:48:48 by avinals          ###   ########.fr       */
+/*   Updated: 2025/07/13 18:30:00 by avinals          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 static char	*g_buffer = NULL;
+
+void	reset_buffer_state(int *buffer_len, int *buffer_size,
+			int *bit_index, unsigned char *current_char)
+{
+	if (g_buffer)
+		free(g_buffer);
+	g_buffer = NULL;
+	*buffer_len = 0;
+	*buffer_size = 0;
+	*bit_index = 0;
+	*current_char = 0;
+}
+
+void	expand_buffer(int *buffer_size)
+{
+	char	*new_buffer;
+	size_t	current_len;
+
+	if (*buffer_size == 0)
+		*buffer_size = 1024;
+	else
+		*buffer_size = *buffer_size * 2;
+	new_buffer = malloc(*buffer_size);
+	if (!new_buffer)
+		return ;
+	if (g_buffer)
+	{
+		current_len = ft_strlen(g_buffer);
+		ft_memcpy(new_buffer, g_buffer, current_len);
+		free(g_buffer);
+	}
+	g_buffer = new_buffer;
+}
+
+void	process_complete_char(unsigned char c, int *buffer_len,
+		int *buffer_size)
+{
+	if (c == '\0')
+	{
+		if (g_buffer && *buffer_len > 0)
+		{
+			g_buffer[*buffer_len] = '\0';
+			ft_printf("%s\n", g_buffer);
+		}
+		else
+			ft_printf("\n");
+		if (g_buffer)
+			free(g_buffer);
+		g_buffer = NULL;
+		*buffer_len = 0;
+		*buffer_size = 0;
+	}
+	else
+	{
+		if (*buffer_len >= *buffer_size)
+			expand_buffer(buffer_size);
+		if (g_buffer)
+			g_buffer[(*buffer_len)++] = c;
+	}
+}
 
 void	ft_handler(int signal, siginfo_t *info, void *context)
 {
@@ -22,51 +82,20 @@ void	ft_handler(int signal, siginfo_t *info, void *context)
 	static int				buffer_size = 0;
 
 	(void)context;
-	current_char = (current_char << 1) | (signal == SIGUSR1);
+	if (signal == SIGUSR1)
+		current_char = (current_char << 1) | 1;
+	else
+		current_char = current_char << 1;
 	bit_index++;
 	if (bit_index == 8)
 	{
-		if (current_char == '\0')
-		{
-			if (g_buffer)
-			{
-				g_buffer[buffer_len] = '\0';
-				ft_printf("%s\n", g_buffer);
-				free(g_buffer);
-				g_buffer = NULL;
-			}
-			else
-				ft_printf("\n");
-			buffer_len = 0;
-			buffer_size = 0;
-		}
+		if (g_buffer == NULL && current_char != '\0')
+			expand_buffer(&buffer_size);
+		if (g_buffer || current_char == '\0')
+			process_complete_char(current_char, &buffer_len, &buffer_size);
 		else
-		{
-			if (buffer_len >= buffer_size)
-			{
-				buffer_size = buffer_size == 0 ? 1024 : buffer_size * 2;
-				char *new_buffer = malloc(buffer_size);
-				if (!new_buffer)
-				{
-					ft_printf("\n[ERROR] Memory allocation failed\n");
-					if (g_buffer)
-						free(g_buffer);
-					g_buffer = NULL;
-					buffer_len = 0;
-					buffer_size = 0;
-					bit_index = 0;
-					current_char = 0;
-					return ;
-				}
-				if (g_buffer)
-				{
-					ft_memcpy(new_buffer, g_buffer, buffer_len);
-					free(g_buffer);
-				}
-				g_buffer = new_buffer;
-			}
-			g_buffer[buffer_len++] = current_char;
-		}
+			reset_buffer_state(&buffer_len, &buffer_size,
+				&bit_index, &current_char);
 		bit_index = 0;
 		current_char = 0;
 	}

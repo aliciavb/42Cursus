9db0032e317ff3919ccf7509ac46b80d27157b3c
Alicia VB <ali.vinals@gmail.com>
2025-07-25 15:07:22 +0200

    modified animation loop and time

--- so_long/game/game_animations.c
+++ so_long/game/game_animations.c
@@ -6,32 +6,33 @@
 /*   By: avinals <avinals-@student.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 01:00:00 by avinals-          #+#    #+#             */
-/*   Updated: 2025/07/25 14:55:01 by avinals          ###   ########.fr       */
+/*   Updated: 2025/07/25 15:05:43 by avinals          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	init_animation_system(t_game *game)
+void init_animation_system(t_game *game)
 {
 	game->animation_frame = 0;
 	game->animation_counter = 0;
 }
 
-void	update_enemy_animation(t_game *game)
+void update_enemy_animation(t_game *game)
 {
 	game->animation_counter++;
-	if (game->animation_counter >= 240)
+	if (game->animation_counter >= 2400)
 	{
 		game->animation_frame = (game->animation_frame + 1) % 3;
 		game->animation_counter = 0;
+		render_map(game);
 	}
 }
 
-int	has_enemies_on_map(t_game *game)
+int has_enemies_on_map(t_game *game)
 {
-	int	x;
-	int	y;
+	int x;
+	int y;
 
 	y = 0;
 	while (y < game->height)
@@ -48,19 +49,16 @@ int	has_enemies_on_map(t_game *game)
 	return (0);
 }
 
-int	animation_loop(t_game *game)
+int animation_loop(t_game *game)
 {
 	if (has_enemies_on_map(game))
-	{
 		update_enemy_animation(game);
-		render_map(game);
-	}
 	return (0);
 }
 
-void	cleanup_animation_frames(t_game *game)
+void cleanup_animation_frames(t_game *game)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (i < 3)
